b7fd0e33d8f33a6368fa8764be8b68facbc4cdfb
chaim <chaimyisroel@gmail.com>
2021-09-02 13:32:35 -0400

    commit tree 02bda14c6eb738964f232eb9c4dbce633bdd0097

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,30 +3,56 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+function ShoppingList({ items, newItems }) {
+	const [selectedCategory, setSelectedCategory] = useState("All");
+	const [onSearch, setSearch] = useState("");
+	const [newArray, setArray] = useState(items);
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
-  }
+	function onSearchChange(e) {
+		//console.log(e);
+		setSearch(e);
+	}
+	function handleCategoryChange(event) {
+		setSelectedCategory(event);
+	}
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+	function onItemFormSubmit(e) {
+		//console.log(e);
+		setArray([...newArray, e]);
+	}
+	const itemsToDisplay = newArray
+		.filter((item) => {
+			if (selectedCategory === "All") return true;
 
-    return item.category === selectedCategory;
-  });
+			return item.category === selectedCategory;
+		})
+		.filter((item) => {
+			if (onSearch === "") {
+				return true;
+			} else {
+				return item.name.includes(onSearch);
+			}
+		});
 
-  return (
-    <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
-      <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div className="ShoppingList">
+			<ItemForm onItemFormSubmit={onItemFormSubmit} />
+			<Filter
+				search={onSearch}
+				onCategoryChange={handleCategoryChange}
+				onSearchChange={onSearchChange}
+			/>
+			<ul className="Items">
+				{itemsToDisplay.map((item) => (
+					<Item
+						key={item.id}
+						name={item.name}
+						category={item.category}
+					/>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default ShoppingList;
